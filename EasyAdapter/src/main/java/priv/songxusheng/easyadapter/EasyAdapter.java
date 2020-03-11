f8893890e0aa084df345efb7d9581b3b71a23217
isong0623 <1099496903@qq.com>
2020-03-11 19:36:37 +0800

    2020年3月11日19:36:19 version 1.0-androidx

---
+++ EasyAdapter/src/main/java/priv/songxusheng/easyadapter/EasyAdapter.java
@@ -0,0 +1,86 @@
+package priv.songxusheng.easyadapter;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.LayoutRes;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * @作者 宋旭升
+ */
+public final class EasyAdapter<T> extends RecyclerView.Adapter<EasyViewHolder>  {
+    public interface IEasyAdapter<T>{
+        void convert(EasyViewHolder holder, T data, int position);
+    }
+    private Context context;
+    private int layoutResID;
+    private boolean bIsUseCache = true;
+    private IEasyAdapter iEasyAdapter;
+    private List<T> lstDatas;
+    private Handler handler = new Handler();
+
+    private void convert(EasyViewHolder holder, T data, int position){
+        iEasyAdapter.convert(holder,data,position);
+    }
+
+    private EasyAdapter(Context context, @LayoutRes int layoutResID, List<T> lstDatas){
+        this.context = context;
+        this.layoutResID = layoutResID;
+        this.lstDatas = lstDatas;
+    }
+
+    public EasyAdapter(Context context, @LayoutRes int layoutResID, List<T> lstDatas,IEasyAdapter<T> iEasyAdapter){
+        this(context,layoutResID,lstDatas);
+        this.iEasyAdapter = iEasyAdapter;
+    }
+
+    public EasyAdapter(Context context, @LayoutRes int layoutResID, List<T> lstDatas,IEasyAdapter<T> iEasyAdapter,boolean isUseCache){
+        this(context,layoutResID,lstDatas,iEasyAdapter);
+        setCacheEnabled(isUseCache);
+    }
+
+    public EasyAdapter setCacheEnabled(boolean isUserCache){
+        setHasStableIds(!(bIsUseCache=isUserCache));
+        return this;
+    }
+
+    @Override
+    public EasyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new EasyViewHolder(LayoutInflater.from(context).inflate(layoutResID,parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(final EasyViewHolder holder,final int position) {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try { convert(holder, lstDatas.get(position),position); } catch (Exception e) {e.printStackTrace();
+                    Log.e("EasyAdapter",e.getMessage()); }
+            }
+        }, 10);
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        if(!bIsUseCache&&recyclerView!=null){
+            recyclerView.getRecycledViewPool().setMaxRecycledViews(0,0);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return lstDatas==null?0:lstDatas.size();
+    }
+}
---
+++ EasyAdapter/src/main/java/priv/songxusheng/easyadapter/EasyViewHolder.java
@@ -0,0 +1,95 @@
+package priv.songxusheng.easyadapter;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.DrawableRes;
+import androidx.annotation.IdRes;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class EasyViewHolder extends RecyclerView.ViewHolder{
+    private View vRoot;
+    public EasyViewHolder(View itemView) {
+        super(itemView);
+        vRoot = itemView;
+//        setIsRecyclable(false);
+    }
+
+    public final <T extends View> T getView(@IdRes int id){
+        return vRoot.findViewById(id);
+    }
+
+    public <T> T getViewAs(@IdRes int id,T defaultView){
+        try {
+            return (T)vRoot.findViewById(id);
+        } catch (Exception e) {
+            return defaultView;
+        }
+    }
+
+    public ImageView getViewAsImageView(@IdRes int id){
+        try{ return (ImageView)getView(id); }catch (Exception e){e.printStackTrace();
+            Log.e("EasyViewHolder",e.getMessage());}
+        return null;
+    }
+
+    public EditText getViewAsEditText(@IdRes int id){
+        try{ return (EditText)getView(id); }catch (Exception e){e.printStackTrace();
+            Log.e("EasyViewHolder",e.getMessage());}
+        return null;
+    }
+
+    public TextView getViewAsTextView(@IdRes int id){
+        try{ return (TextView)getView(id); }catch (Exception e){e.printStackTrace();
+            Log.e("EasyViewHolder",e.getMessage());}
+        return null;
+    }
+
+    public void setOnClickListener(@IdRes int id , View.OnClickListener listener){
+        try { getView(id).setOnClickListener(listener); } catch (Exception e) { }
+    }
+
+    public void setText(@IdRes int id , String text){
+        try { getViewAsTextView(id).setText(text);} catch (Exception e) {}
+    }
+
+    public void setImageDrawable(@IdRes int id, Drawable drawable){
+        try { getViewAsImageView(id).setImageDrawable(drawable);} catch (Exception e) {}
+    }
+
+    public void setTextColor(@IdRes int id , int color){
+        try { getViewAsTextView(id).setTextColor(color);} catch (Exception e) {}
+    }
+
+    public void setBackgroundRes(@IdRes int id , @DrawableRes int res){
+        try { getView(id).setBackgroundResource(res); } catch (Exception e) { }
+    }
+    public void setVisible(@IdRes int id, boolean visible){
+        try { getView(id).setVisibility(visible? View.VISIBLE: View.GONE); } catch (Exception e) { }
+    }
+
+    public void setVisibility(@IdRes int id, int visibility){
+        try { getView(id).setVisibility(visibility); } catch (Exception e) { }
+    }
+
+    public void setImageResource(@IdRes int id , @DrawableRes int res){
+        try { getViewAsImageView(id).setImageResource(res);} catch (Exception e) {}
+    }
+
+    public void setBackgroundColor(@IdRes int id, int color){
+        try { getView(id).setBackgroundColor(color); } catch (Exception e) { }
+    }
+
+    public void setTag(@IdRes int id, Object tag){
+        try { getView(id).setTag(tag); } catch (Exception e) { }
+    }
+
+    public void setImageBitmap(@IdRes int id, Bitmap bitmap){
+        try { getViewAsImageView(id).setImageBitmap(bitmap);} catch (Exception e) {}
+    }
+}
